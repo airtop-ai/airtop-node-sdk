8ebccbe43a1ed0040f82a67cd02a88dffb5a7708
kvindascr <kivind@gmail.com>
2024-09-26 15:11:35 -0600

    Adding augmented code on AirtopClient.

---
+++ src/wrapper/AirtopClient.ts
@@ -0,0 +1,41 @@
+import { AirtopClient as FernClient } from '../Client'; // alias the Fern generated client
+import { AirtopWindows } from './AirtopWindows';
+import { AirtopSessions } from './AirtopSessions';
+
+type AugmentedOptions = FernClient.Options & { debug?: boolean };
+
+export class AirtopClient {
+  public debug: boolean;
+  private _client: FernClient;
+  private _windowsWrapper: AirtopWindows;
+  private _sessionsWrapper: AirtopSessions;
+
+  constructor(options?: AugmentedOptions) {
+    this._client = new FernClient(options);
+    this.debug = options?.debug || false;
+    this._windowsWrapper = new AirtopWindows(this._client);
+    this._sessionsWrapper = new AirtopSessions(this._client, options?.debug);
+  }
+
+  get windows() {
+    return this._windowsWrapper;
+  }
+
+  get sessions() {
+    return this._sessionsWrapper;
+  }
+
+  get profiles() {
+    return this._client.profiles;
+  }
+
+  log(message: string) {
+    if (this.debug) {
+      console.log(message);
+    }
+  }
+
+  error(err: any) {
+    console.error(err);
+  }
+}
---
+++ src/wrapper/AirtopWindows.ts
@@ -0,0 +1,91 @@
+import { Page as PuppeteerPage } from 'puppeteer';
+import { Page as PlaywrightPage } from 'playwright';
+import { WebDriver } from 'selenium-webdriver';
+import { AirtopClient as FernClient } from '../Client';
+import * as Airtop from '../api';
+import { Windows as WindowsClass, Windows as WindowsNamespace } from '../api/resources/windows/client/Client';
+
+export class AirtopWindows {
+  private _windows: WindowsClass;
+
+  constructor(private airtopClient: FernClient) {
+    this._windows = airtopClient.windows;
+  }
+
+  async getWindowInfo(
+    sessionId: string,
+    windowId: string,
+    request?: Airtop.GetWindowInfoRequest,
+    requestOptions?: WindowsNamespace.RequestOptions,
+  ): Promise<Airtop.WindowEnvelopeDefaultMetaWrapper> {
+    return this._windows.getWindowInfo(sessionId, windowId, request, requestOptions);
+  }
+
+  async promptContent(
+    sessionId: string,
+    windowId: string,
+    request: Airtop.PromptContentRequest,
+    requestOptions?: WindowsNamespace.RequestOptions,
+  ): Promise<Airtop.AiResponseEnvelopeExternalSessionAiResponseMetadataWrapper> {
+    return this._windows.promptContent(sessionId, windowId, request, requestOptions);
+  }
+
+  scrapeContent(
+    sessionId: string,
+    windowId: string,
+    requestOptions?: Airtop.ScrapeContentRequest,
+  ): Promise<Airtop.ScrapeResponseEnvelopeExternalSessionAiResponseMetadataWrapper> {
+    return this._windows.scrapeContent(sessionId, windowId, requestOptions);
+  }
+
+  summarizeContent(
+    sessionId: string,
+    windowId: string,
+    request?: Airtop.SummarizeContentRequest,
+    requestOptions?: WindowsNamespace.RequestOptions,
+  ): Promise<Airtop.AiResponseEnvelopeExternalSessionAiResponseMetadataWrapper> {
+    return this._windows.summarizeContent(sessionId, windowId, request, requestOptions);
+  }
+
+  async getWindowInfoForPuppeteerPage(
+    sessionId: string,
+    page: PuppeteerPage,
+    request?: Airtop.GetWindowInfoRequest,
+    requestOptions?: WindowsNamespace.RequestOptions,
+  ): Promise<Airtop.WindowEnvelopeDefaultMetaWrapper> {
+    const targetId = await (page.mainFrame() as any)._id;
+    return await this.getWindowInfo(sessionId, targetId, request, requestOptions);
+  }
+
+  async getWindowInfoForPlaywrightPage(
+    sessionId: string,
+    page: PlaywrightPage,
+    request?: Airtop.GetWindowInfoRequest,
+    requestOptions?: WindowsNamespace.RequestOptions,
+  ): Promise<Airtop.WindowEnvelopeDefaultMetaWrapper> {
+    // Retrieve target information
+    const cdpSession = await page.context().newCDPSession(page);
+    const { targetInfo } = await cdpSession.send('Target.getTargetInfo');
+    const targetId = targetInfo.targetId;
+    if (!targetId) {
+      throw new Error('TargetId not found');
+    }
+    return await this.getWindowInfo(sessionId, targetId, request, requestOptions);
+  }
+
+  async getWindowInfoForSeleniumDriver(
+    sessionId: string,
+    driver: WebDriver,
+    request?: Airtop.GetWindowInfoRequest,
+    requestOptions?: WindowsNamespace.RequestOptions,
+  ): Promise<Airtop.WindowEnvelopeDefaultMetaWrapper> {
+    const cdpSession = await driver.createCDPConnection('page');
+    // Fetch the available targets using the Target domain
+    const { targetInfo } = await cdpSession.send('Target.getTargetInfo');
+    const targetId = targetInfo.targetId;
+    if (!targetId) {
+      throw new Error('TargetId not found');
+    }
+    return await this.getWindowInfo(sessionId, targetId, request, requestOptions);
+  }
+}
