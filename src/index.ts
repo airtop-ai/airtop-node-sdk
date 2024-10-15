ca77d0044ee64bacade7dfa47fe46001c8cd054c
Jim Cullison <jim.cullison@gmail.com>
2024-10-15 10:27:23 -0600

    test dev process

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export { AirtopEnvironment } from "./environments";
 export { AirtopError, AirtopTimeoutError } from "./errors";
 export { AirtopClient } from './wrapper/AirtopClient';
 export { LiveViewEventName, LiveViewMessage } from './live-view/types';
+
+console.log('loading airtop sdk')
