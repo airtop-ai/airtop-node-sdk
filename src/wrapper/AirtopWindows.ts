3d271fb1c2edfc6c5a95a448c254913048df8111
kvindascr <kivind@gmail.com>
2025-02-24 16:33:31 -0600

    Fix hover typing.

--- src/wrapper/AirtopWindows.ts
+++ src/wrapper/AirtopWindows.ts
@@ -242,7 +242,7 @@ export class AirtopWindows extends WindowsClass {
   public async hover(
     sessionId: string,
     windowId: string,
-    request: Airtop.SessionHoverHandlerRequestBody = {},
+    request: Airtop.SessionHoverHandlerRequestBody,
     requestOptions?: WindowsNamespace.RequestOptions,
   ): Promise<Airtop.AiPromptResponse> {
     return super.hover(sessionId, windowId, request, {
