aedabc69a235416de03f8c30de86fa3c0c45add7
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-04-23 18:50:38 +0000

    SDK regeneration

--- src/serialization/resources/sessions/types/SessionsEventsResponse.ts
+++ src/serialization/resources/sessions/types/SessionsEventsResponse.ts
@@ -5,24 +5,24 @@
 import * as serializers from "../../../index";
 import * as Airtop from "../../../../api/index";
 import * as core from "../../../../core";
-import { SessionsEventsResponseSessionEvent } from "./SessionsEventsResponseSessionEvent";
-import { SessionsEventsResponseFileEvent } from "./SessionsEventsResponseFileEvent";
-import { SessionsEventsResponseCaptchaEvent } from "./SessionsEventsResponseCaptchaEvent";
 import { SessionsEventsResponseStatus } from "./SessionsEventsResponseStatus";
 import { SessionsEventsResponseError } from "./SessionsEventsResponseError";
 import { SessionsEventsResponseWindowEvent } from "./SessionsEventsResponseWindowEvent";
+import { SessionsEventsResponseSessionEvent } from "./SessionsEventsResponseSessionEvent";
+import { SessionsEventsResponseFileEvent } from "./SessionsEventsResponseFileEvent";
+import { SessionsEventsResponseCaptchaEvent } from "./SessionsEventsResponseCaptchaEvent";
 
 export const SessionsEventsResponse: core.serialization.Schema<
     serializers.SessionsEventsResponse.Raw,
     Airtop.SessionsEventsResponse
 > = core.serialization
     .union("event", {
-        sessionEvent: SessionsEventsResponseSessionEvent,
-        fileEvent: SessionsEventsResponseFileEvent,
-        captchaEvent: SessionsEventsResponseCaptchaEvent,
         status: SessionsEventsResponseStatus,
         error: SessionsEventsResponseError,
         windowEvent: SessionsEventsResponseWindowEvent,
+        sessionEvent: SessionsEventsResponseSessionEvent,
+        fileEvent: SessionsEventsResponseFileEvent,
+        captchaEvent: SessionsEventsResponseCaptchaEvent,
     })
     .transform<Airtop.SessionsEventsResponse>({
         transform: (value) => value,
@@ -31,24 +31,12 @@ export const SessionsEventsResponse: core.serialization.Schema<
 
 export declare namespace SessionsEventsResponse {
     export type Raw =
-        | SessionsEventsResponse.SessionEvent
-        | SessionsEventsResponse.FileEvent
-        | SessionsEventsResponse.CaptchaEvent
         | SessionsEventsResponse.Status
         | SessionsEventsResponse.Error
-        | SessionsEventsResponse.WindowEvent;
-
-    export interface SessionEvent extends SessionsEventsResponseSessionEvent.Raw {
-        event: "sessionEvent";
-    }
-
-    export interface FileEvent extends SessionsEventsResponseFileEvent.Raw {
-        event: "fileEvent";
-    }
-
-    export interface CaptchaEvent extends SessionsEventsResponseCaptchaEvent.Raw {
-        event: "captchaEvent";
-    }
+        | SessionsEventsResponse.WindowEvent
+        | SessionsEventsResponse.SessionEvent
+        | SessionsEventsResponse.FileEvent
+        | SessionsEventsResponse.CaptchaEvent;
 
     export interface Status extends SessionsEventsResponseStatus.Raw {
         event: "status";
@@ -60,5 +48,17 @@ export declare namespace SessionsEventsResponse {
 
     export interface WindowEvent extends SessionsEventsResponseWindowEvent.Raw {
         event: "windowEvent";
+    }
+
+    export interface SessionEvent extends SessionsEventsResponseSessionEvent.Raw {
+        event: "sessionEvent";
+    }
+
+    export interface FileEvent extends SessionsEventsResponseFileEvent.Raw {
+        event: "fileEvent";
+    }
+
+    export interface CaptchaEvent extends SessionsEventsResponseCaptchaEvent.Raw {
+        event: "captchaEvent";
     }
 }
